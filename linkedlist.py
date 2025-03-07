c7664fd103358a879d9a3344cc1c2598407c759d
Karen Chahrour <karenchahrour123@gmail.com>
2025-03-07 13:05:35 -0800

    Creating append method

--- linkedlist.py
+++ linkedlist.py
@@ -5,10 +5,20 @@ class Node:
         self.next = None
 
 class LinkedList:
-    
+
     def __init__(self):
         self.head = None
         self.length = 0
 
-    
+    def append(self, node):
         
+        if self.length == 0:
+            self.head = node
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            
+            current.next = node
+        
+        self.length +=1
