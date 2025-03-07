d4fab35846dbc25b65662f7b7c60aef77cb1234e
Karen Chahrour <karenchahrour123@gmail.com>
2025-03-07 14:53:26 -0800

    Implementing delete  method

--- linkedlist.py
+++ linkedlist.py
@@ -23,6 +23,7 @@ class LinkedList:
         
         self.length +=1
 
+
     def printlist(self):
 
         current = self.head
@@ -30,10 +31,36 @@ class LinkedList:
             print(current.data)
             current = current.next
 
-linkedlist1 = LinkedList()
-linkedlist1.append(Node(15))
-linkedlist1.append(Node(3))
-linkedlist1.append(Node(22))
-linkedlist1.append(Node(40))
+    
+    def prepend(self, node):
 
+        if self.length == 0:
+            self.head = node
+        else:
+            node.next = self.head  # Point new node to the current head
+            self.head = node  
+                            
+    def delete(self, data):
+
+        current = self.head
+        precurrent = None
+        target = None
+        while current != None:
+            if current.data == data:
+                target = current.data
+                break
+            else:
+                precurrent = current
+                current = current.next
+
+        precurrent.next = current.next           
+        current = None
+        return target
+
+linkedlist1 = LinkedList()
+linkedlist1.append(Node(10))
+linkedlist1.append(Node(20))
+linkedlist1.append(Node(30))
+linkedlist1.prepend(Node(5))
+print(f"You removed {linkedlist1.delete(20)}")
 linkedlist1.printlist()
